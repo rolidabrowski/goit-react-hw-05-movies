91b1c92ef9a5fe3ac5d472de905dce38dd0798ef
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-07-07 17:10:34 +0200

    added cast

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,21 +1,51 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMoviesCast } from '../../services/themoviedbAPI';
+
 export const Cast = () => {
-  return (
-    <section>
-      <h2>Our mission</h2>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut, nesciunt
-        veniam. Excepturi itaque, voluptates fugiat perspiciatis quo saepe! Iste
-        eaque porro eveniet error dicta, modi ipsum hic quis minima inventore.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quaerat
-        illum excepturi odit doloremque, vitae quasi corporis commodi nisi quae
-        perspiciatis amet consectetur reprehenderit inventore laborum facilis
-        quia mollitia exercitationem eaque rerum dignissimos maiores, quos iure
-        blanditiis. Dolorem, nam? Aliquid sequi molestias vel, tenetur maxime
-        pariatur? Molestiae libero cum quidem.
-      </p>
-    </section>
-  );
+  const [movieCast, setMovieCast] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsLoading(true);
+        const data = await getMoviesCast(movieId);
+        setMovieCast(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [movieId]);
+
+  if (error) {
+    return alert('Something went wrong. Try again.');
+  } else if (isLoading) {
+    return <p>Loading...</p>;
+  } else if (!movieCast) {
+    return <p>Sorry, no cast for this movie.</p>;
+  } else {
+    return (
+      <section>
+        <ul>
+          {movieCast.map(({ id, name, character, profile_path }) => (
+            <li key={id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${profile_path}`}
+                alt={name}
+                width="200"
+              />
+              <h3>{name}</h3>
+              <p>Character: {character}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
 };
 export default Cast;
